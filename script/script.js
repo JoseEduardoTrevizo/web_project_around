dad28a52d8bd3905ef0b8634145cb9a34e37a994
EduardoTrevizo <Edwardo_tp@hotmail.com>
2024-05-22 17:23:31 -0600

    Add funtion button the trash and like

--- script/script.js
+++ script/script.js
@@ -8,6 +8,58 @@ const profileAbout = document.querySelector(".profile-info__avatar_ocupation");
 const inputName = document.querySelector("#input_name");
 const inputAbout = document.querySelector("#input_about");
 const formProfile = document.querySelector(".popup__container");
+const template = document.querySelector(".template-card");
+const cardArea = document.querySelector(".elements");
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+function cardGenerator(name, link) {
+  const card = template.cloneNode(true).content.querySelector(".element");
+  const cardImage = card.querySelector(".elements-card__element_image");
+  const cardTitle = card.querySelector(".elements-name__place");
+  const btnDelate = card.querySelector(".elements-card__element_trash");
+  const btnLike = card.querySelector(".elements-name__place_like");
+  //Funcion para crear el efecto de like
+  btnLike.addEventListener("click", function () {
+    btnLike.classList.toggle("elements-name__place_like_active");
+  });
+  //Funcion para remover la card
+  btnDelate.addEventListener("click", function () {
+    card.remove();
+  });
+  cardImage.src = link;
+  cardTitle.textContent = name;
+  cardImage.alt = name;
+  cardArea.append(card);
+}
+initialCards.forEach(function (element) {
+  //bucle que se itera la cantidad de veces por los elementos que contenga el array
+  cardGenerator(element.name, element.link);
+});
 
 boton.addEventListener("click", function (evt) {
   modal.style.display = "block";
